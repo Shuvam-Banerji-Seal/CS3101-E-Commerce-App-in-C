294558c9775342a8a1cd375e28e4129f65c5e61e
Shuvam Banerji Seal <shuvambanerjiseal@gmail.com>
2024-11-20 05:51:12 +0530

    removed garbage

--- build/CMakeFiles/main.dir/auth.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/main.dir/auth.c.o: \
- /home/shuvam/codes/CS3101-E-Commerce-App-in-C/auth.c \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/masterchatbot.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for masterchatbot.
-# This may be replaced when dependencies are built.
